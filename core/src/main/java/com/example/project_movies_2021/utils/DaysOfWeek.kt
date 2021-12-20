9e40ae320b2ea3a0e1b58dc91e396cbd3f100b0c
Enzo <enzo.carvalho.teles@gmail.com>
2021-12-20 14:02:03 -0300

    inserindo máscara de data e dinheiro no projeto

---
+++ core/src/main/java/com/example/project_movies_2021/utils/DaysOfWeek.kt
@@ -0,0 +1,11 @@
+package com.example.project_movies_2021.utils
+
+enum class DaysOfWeek(val day: String) {
+    MONDAY("MONDAY"),
+    TUESDAY("TUESDAY"),
+    WEDNESDAY("WEDNESDAY"),
+    THURSDAY("THURSDAY"),
+    FRIDAY("FRIDAY"),
+    SATURDAY("SATURDAY"),
+    SUNDAY("SUNDAY")
+}
